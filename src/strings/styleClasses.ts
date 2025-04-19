14f48055b4b698e221f6edad75ba1057d44b6a72
Daniel Salazar <daniel.salazar.vaca@gmail.com>
2025-04-19 06:50:13 -0500

    Texturas de fondo
    Se agregaron texturas para los fondos y se adaptaron los estilos

--- src/strings/styleClasses.ts
+++ src/strings/styleClasses.ts
@@ -1,10 +1,10 @@
 export const styleClasses = {
     title: "text-xl font-bold text-center text-[#f2f3f4]",
-    navbar: "bg-[#252539] h-12 content-center ",
+    navbar: "bg-[#2a2a2a] h-12 content-center bg-[url(src/assets/Texture.webp)] bg-blend-overlay",
     container: "my-5",
     selections: "flex flex-wrap justify-center",
     selectionComponent: "mx-20 my-5 cursor-pointer",
-    containerSelectionTitle: "my-20 text-xl font-bold text-center text-[#f2f3f4]",
+    containerSelectionTitle: "my-20 text-5xl font-bold text-center text-[#f2f3f4]",
     selectionImg: "w-40 drop-shadow-cyan-700",
     selectionTitle: "text-center text-[#f2f3f4] my-3 text-lg font-semibold text-shadow-cyan-700",
 }
